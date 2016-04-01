8220bc7a4275c181a0f4d3cac6c2c6e398e38bfd
Ben McGahee <ben.mcgahee@gmail.com>
2016-04-01 19:53:04 -0400

    Added files via upload

---
+++ Shapes.java
@@ -0,0 +1,24 @@
+package shapes;
+
+//Author: Ben McGahee
+//Title: Shapes Project
+//Date: 4/1/2016
+//Purpose: The Shapes Project allows one to define properties and behaviors of various shapes.  
+//The Circle class is a subclass of the Shapes class that allows one to calculate different aspects of the circle
+//such as the circumference, area, seeing if a point lies within or on the circle, etc.  
+
+public class Shapes {
+    
+    static String name;
+    static String color;
+    double area;
+    
+    public String getName(String name){
+        return name;
+    }
+    
+    public String getColor(String color){
+        return color;
+    }
+}
+
